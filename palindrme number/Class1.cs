f37488d58342cb8e5170be73813c994946f683e3
Asus <Asus@ARSHIA>
2023-10-06 18:00:48 +0330

    Add project files.

---
+++ palindrme number/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrme_number
+{
+    internal class Class1
+    {
+    }
+}
---
+++ palindrme number/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrme_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Palindrome number in the range of zero to one thousand:");
+
+            for (int i = 200; i <= 500; i++)
+            {
+                if (ispalindrome(i))
+                {
+                    Console.WriteLine(i);
+
+                }
+
+
+            }
+        }
+        public static bool ispalindrome(int number)
+        {
+            int originalNumber = number;
+            int reverseNumber = 0;
+
+            while (number > 0)
+            {
+                int remainder = number % 10;
+                reverseNumber = (reverseNumber * 10) + remainder;
+                number = number / 10;
+            }
+
+
+
+
+            return originalNumber == reverseNumber;
+
+        }
+    }
+}
+
+
+
